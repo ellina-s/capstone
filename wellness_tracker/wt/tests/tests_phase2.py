0d5c4ccdfa1f24843dac6043e049548ae59ca10d
ellina-s <ellina.sergeyeva@gmail.com>
2015-03-01 19:29:02 -0800

    Added a test for missing the input info in create patient form

--- wellness_tracker/wt/tests/tests_phase2.py
+++ wellness_tracker/wt/tests/tests_phase2.py
@@ -109,6 +109,36 @@ class WTBaseTest(unittest.TestCase):
         assert "Missing a username, password, or email." not in driver.page_source
         assert "Email error" not in driver.page_source
 
+    # Test for missing information in the create patient form 
+    def test_create_patient_missing_info(self):
+        driver = self.driver
+        driver.get(self.base_url + "/login/")
+        self.assertIn("WellnessTracker - Login", driver.title)
+        driver.find_element_by_id("id_username").clear()
+        driver.find_element_by_id("id_username").send_keys("doctoraj")
+        driver.find_element_by_id("id_password").clear()
+        driver.find_element_by_id("id_password").send_keys("ajp")
+        driver.find_element_by_xpath("//button[@type='submit']").click()
+        driver.find_element_by_link_text("Add Patient").click()
+        # Check that status messages are not present
+        assert "Patient has been created successfully" not in driver.page_source
+        assert "Missing a username, password, or email." not in driver.page_source
+        assert "Duplicate username. Please select another username." not in driver.page_source
+        assert "Email error" not in driver.page_source
+        # Fill in the data and submit
+        driver.find_element_by_id("userid").clear()
+        driver.find_element_by_id("userid").send_keys("")
+        driver.find_element_by_id("useremail").clear()
+        driver.find_element_by_id("useremail").send_keys("")
+        driver.find_element_by_id("password").clear()
+        driver.find_element_by_id("password").send_keys("")
+        driver.find_element_by_xpath("//input[@type='submit']").click()
+        #Check that the missing info error is caught
+        assert "Missing a username, password, or email." in driver.page_source
+        assert "Duplicate username. Please select another username." not in driver.page_source
+        assert "Patient has been created successfully" not in driver.page_source
+        assert "Email error" not in driver.page_source
+
     def tearDown(self):
         self.driver.close()
         
